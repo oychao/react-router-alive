f928b0d34624dafd520a9a12269c6e9bf40ec7a8
ouyang-chao <chao@ouyang.io>
2019-03-25 00:29:48 +0800

    [feature] use utils in react-router

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import { isValidElementType } from 'react-is';
 import PropTypes from 'prop-types';
 import invariant from 'tiny-invariant';
 import warning from 'tiny-warning';
-import { __RouterContext as RouterContext } from 'react-router';
-
-import matchPath from './matchPath';
+import { matchPath, __RouterContext as RouterContext } from 'react-router';
 
 function isEmptyChildren(children) {
   return React.Children.count(children) === 0;
@@ -97,65 +95,6 @@ class Route extends React.Component {
 //       componentInstance.child.stateNode.style.display = 'none';
 //     }
 //   }
-// }
-
-// if (__DEV__) {
-//   Route.propTypes = {
-//     children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]),
-//     component: (props, propName) => {
-//       if (props[propName] && !isValidElementType(props[propName])) {
-//         return new Error(
-//           'Invalid prop \'component\' supplied to \'Route\': the prop is not a valid React component'
-//         );
-//       }
-//     },
-//     exact: PropTypes.bool,
-//     location: PropTypes.object,
-//     path: PropTypes.oneOfType([
-//       PropTypes.string,
-//       PropTypes.arrayOf(PropTypes.string)
-//     ]),
-//     render: PropTypes.func,
-//     sensitive: PropTypes.bool,
-//     strict: PropTypes.bool
-//   };
-
-//   Route.prototype.componentDidMount = function() {
-//     warning(
-//       !(
-//         this.props.children &&
-//         !isEmptyChildren(this.props.children) &&
-//         this.props.component
-//       ),
-//       'You should not use <Route component> and <Route children> in the same route; <Route component> will be ignored'
-//     );
-
-//     warning(
-//       !(
-//         this.props.children &&
-//         !isEmptyChildren(this.props.children) &&
-//         this.props.render
-//       ),
-//       'You should not use <Route render> and <Route children> in the same route; <Route render> will be ignored'
-//     );
-
-//     warning(
-//       !(this.props.component && this.props.render),
-//       'You should not use <Route component> and <Route render> in the same route; <Route render> will be ignored'
-//     );
-//   };
-
-//   Route.prototype.componentDidUpdate = function(prevProps) {
-//     warning(
-//       !(this.props.location && !prevProps.location),
-//       '<Route> elements should not change from uncontrolled to controlled (or vice versa). You initially used no "location" prop and then provided one on a subsequent render.'
-//     );
-
-//     warning(
-//       !(!this.props.location && prevProps.location),
-//       '<Route> elements should not change from controlled to uncontrolled (or vice versa). You provided a "location" prop initially but omitted it on a subsequent render.'
-//     );
-//   };
 // }
 
 export default Route;
